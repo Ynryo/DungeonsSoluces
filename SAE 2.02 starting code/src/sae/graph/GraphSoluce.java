9afd3bc0f4a2f6cb281d58eb9b5442e9d0fdc388
Ynryo <ewenatideklein@gmail.com>
2025-05-01 23:33:09 +0200

    finished BFS

--- SAE 2.02 starting code/src/sae/graph/GraphSoluce.java
+++ SAE 2.02 starting code/src/sae/graph/GraphSoluce.java
@@ -14,7 +14,7 @@ public class GraphSoluce {
     }
 
     public void add(Node node2add) {
-        soluce.add(node2add);
+        soluce.add(0, node2add); //ajoute le node au debut de la list
     }
 
     public List<Node> getSoluce() {
--- SAE 2.02 starting code/src/sae/solver/SolverWithBFS.java
+++ SAE 2.02 starting code/src/sae/solver/SolverWithBFS.java
@@ -9,9 +9,9 @@ import sae.graph.GraphSoluce;
 import sae.graph.Node;
 
 public class SolverWithBFS extends SolverGeneric {
-	private HashMap<Node, Node> dicoNode;
-	private List<Node> markedNodes;
-	private LinkedList<Node> file;
+    private HashMap<Node, Node> dicoNode;
+    private List<Node> markedNodes;
+    private LinkedList<Node> file;
 
     public SolverWithBFS(Node startingNode, Node endingNode) {
         super(startingNode, endingNode);
@@ -20,34 +20,49 @@ public class SolverWithBFS extends SolverGeneric {
         file = new LinkedList<Node>();
     }
 
-	@Override
-	protected void resolve() {
-		GraphSoluce graphSoluce = getGraphSoluce();
-		Node startingNode = getStartingNode();
-		
-		markedNodes.add(startingNode);
-		file.add(startingNode);
-		while(!file.isEmpty()) {
-			Node s = file.getFirst();  
-			for(Node v: s.neighbors()) {
-				if (!markedNodes.contains(v)) {
-					dicoNode.put(v, s);
-					markedNodes.add(v);
-					file.add(v);
-				}
-			}
-		}
-		
-		Node predecessor = dicoNode.get(getEndingNode());
-		while(dicoNode.get(predecessor) != null) {
-			graphSoluce.add(predecessor);
-			incSteps();
-			predecessor = dicoNode.get(predecessor);
-		}
-	}
-	
-	@Override
-	public String toString() {
-		return "BFS";
-	}
+    @Override
+    protected void resolve() {
+        Node startingNode = getStartingNode();
+        Node endingNode = getEndingNode();
+
+        markedNodes.add(startingNode);
+        file.add(startingNode);
+
+        boolean found = false;
+
+        while(!file.isEmpty() && !found) {
+            Node s = file.poll();  // <-- enlève le premier élément de la file
+
+            for(Node v: s.neighbors()) {
+                if (!markedNodes.contains(v)) {
+                    dicoNode.put(v, s);
+                    markedNodes.add(v);
+                    file.add(v);
+                    if (v.equals(endingNode)) {
+                        found = true;
+                        break;
+                    }
+                }
+            }
+        }
+
+        // Reconstitution du chemin
+        Node current = endingNode;
+		GraphSoluce graphSoluce = new GraphSoluce();
+        while (current != null && dicoNode.containsKey(current)) {
+            graphSoluce.add(current); // ajoute au début
+            current = dicoNode.get(current);
+            incSteps();
+        }
+        if (current == startingNode) {
+            graphSoluce.add(startingNode);
+        }
+
+        // setGraphSoluce(new GraphSoluce(path));
+    }
+
+    @Override
+    public String toString() {
+        return "BFS";
+    }
 }
